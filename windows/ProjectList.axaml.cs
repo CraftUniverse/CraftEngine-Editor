be269a4bf8605c5ffc4ef2267042feecd8417abd
Turboman3000 <turboman@turboman.de>
2025-03-08 22:23:32 +0100

    feat(ui): Add project list window
    Added a new window for displaying a list of projects.
    Moved MainWindow to the windows folder.
    Added an icon.
    Updated the App.axaml.cs to open the ProjectList window on startup.

---
+++ windows/ProjectList.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace dev.craftengine.editor.windows;
+
+public partial class ProjectList : Window
+{
+    public ProjectList()
+    {
+        InitializeComponent();
+    }
+}
