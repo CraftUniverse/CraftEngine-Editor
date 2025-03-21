e15515301ec85e15142a9cec3134e46cbc17d60d
Turboman3000 <turboman@turboman.de>
2025-03-21 01:44:14 +0100

    feat(GameViewer): Replace SDL with Veldrid
    Replaced SDL2 implementation with Veldrid for game viewing.  Created a new Veldrid-based Window class and updated the GameViewer to use it. Removed SDLWindow and related SDL dependencies.

---
+++ src/GameViewer/Window.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Avalonia.Platform;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+
+namespace dev.craftengine.editor.GameViewer;
+
+public class Window
+{
+    private GraphicsDevice _graphicsDevice;
+
+    public Window(IntPtr windowHandle)
+    {
+        var windowCI = new WindowCreateInfo()
+        {
+            WindowTitle = "Veldrid Tutorial"
+        };
+        var window = VeldridStartup.CreateWindow(ref windowCI);
+
+        var options = new GraphicsDeviceOptions
+        {
+            PreferStandardClipSpaceYDirection = true,
+            PreferDepthRangeZeroToOne = true
+        };
+
+        var swapchain = SwapchainSource.CreateWin32(windowHandle, windowHandle);
+
+
+        while (window.Exists)
+        {
+            window.PumpEvents();
+        }
+    }
+}
