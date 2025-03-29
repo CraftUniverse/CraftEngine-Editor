36461f5aefd6f726c0c0ce7d32aba42062d9498a
Turboman3000 <turboman@turboman.de>
2025-03-29 22:50:08 +0100

    refactor(Views): Move About window to root Views folder
    Moved the About window and its associated files from the Views/Panels directory to the root Views directory.  Updated namespaces and project file accordingly.

---
+++ src/Views/About.axaml.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using dev.craftengine.editor.Helpers;
+
+namespace dev.craftengine.editor.Views;
+
+public partial class About : Window
+{
+    public About()
+    {
+        InitializeComponent();
+
+        VersionText.Text = VersionControl.Version;
+        KeyDownEvent.AddClassHandler<TopLevel>(OnKeyDown, handledEventsToo: true);
+    }
+
+    private void OnKeyDown(TopLevel topLevel, KeyEventArgs e)
+    {
+        if (e.Key == Key.Escape)
+        {
+            Close();
+        }
+    }
+}
