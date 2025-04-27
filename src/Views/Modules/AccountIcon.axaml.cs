b12cd120ce634339c3fe33ab98ad020cb722b8a3
Turboman3000 <turboman@turboman.de>
2025-04-27 17:10:28 +0200

    feat(featAccountIcon): Implement login/logout context menu
    Implement a context menu in the AccountIcon(AccountIcon): Implement login/logout context menu
    
    Implement a context menu in the AccountIcon module that displays different options based on login status.  Added logic to show/hide menu items for logged-in and logged-out states.
     module that displays different options based on login status.  Added "LOGGED_IN" and "LOGGED_OUT" groups to the menu items for visibility control.

--- src/Views/Modules/AccountIcon.axaml.cs
+++ src/Views/Modules/AccountIcon.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace dev.craftengine.editor.Views.Modules;
 
@@ -21,6 +22,23 @@ public partial class AccountIcon : UserControl
         else
         {
             border.ContextMenu.Open();
+        }
+    }
+
+    private void Context_OnLoaded(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not ContextMenu context) return;
+
+        foreach (Control? child in context.Items)
+        {
+            if (child!.GetType() != typeof(MenuItem)) continue;
+
+            if (child is not MenuItem item) return;
+
+            if (item.GroupName == "LOGGED_IN")
+            {
+                item.IsVisible = false;
+            }
         }
     }
 }
